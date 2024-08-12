51ac9877102843e5c276b8f477b4440ca8da4971
Decebal Dobrica <352761+decebal@users.noreply.github.com>
2024-08-13 00:16:55 +0100

    feat: deploy and verify token

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const privateKeys = process.env.PRIVATE_KEYS || ""
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -20,6 +12,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost: {}
+    localhost: {},
+    sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_URL,
+      accounts: privateKeys.split(',')
+    },
   },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY
+    }
+  }
+
 };
